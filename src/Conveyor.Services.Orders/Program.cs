03f9332163703ab0123687398815c808dec1c0aa
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-20 06:40:13 +0200

    .NET Core 3.0 sample

--- src/Conveyor.Services.Orders/Program.cs
+++ src/Conveyor.Services.Orders/Program.cs
@@ -1,28 +1,6 @@
-﻿using System;
-using Convey;
-using Convey.CQRS.Commands;
-using Convey.CQRS.Events;
-using Convey.CQRS.Queries;
-using Convey.Discovery.Consul;
-using Convey.HTTP;
-using Convey.LoadBalancing.Fabio;
-using Convey.Logging;
-using Convey.MessageBrokers.CQRS;
-using Convey.MessageBrokers.RabbitMQ;
-using Convey.Metrics.AppMetrics;
-using Convey.Persistence.MongoDB;
-using Convey.Tracing.Jaeger;
-using Convey.Tracing.Jaeger.RabbitMQ;
-using Convey.WebApi;
-using Convey.WebApi.CQRS;
-using Conveyor.Services.Orders.Commands;
-using Conveyor.Services.Orders.Domain;
-using Conveyor.Services.Orders.DTO;
-using Conveyor.Services.Orders.Events.External;
-using Conveyor.Services.Orders.Queries;
+﻿using Convey.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -41,40 +19,11 @@ namespace Conveyor.Services.Orders
             {
                 webBuilder.ConfigureServices(services => services
                         .Configure<KestrelServerOptions>(options => { options.AllowSynchronousIO = true; })
-                        .AddOpenTracing()
-                        .AddConvey()
-                        .AddServices()
-                        .AddHttpClient()
-                        .AddConsul()
-                        .AddFabio()
-                        .AddJaeger()
-                        .AddMongo()
-                        .AddMongoRepository<Order, Guid>("orders")
-                        .AddCommandHandlers()
-                        .AddEventHandlers()
-                        .AddQueryHandlers()
-                        .AddInMemoryCommandDispatcher()
-                        .AddInMemoryEventDispatcher()
-                        .AddInMemoryQueryDispatcher()
-                        .AddRabbitMq(plugins: p => p.AddJaegerRabbitMqPlugin())
-                        .AddMetrics()
-                        .AddWebApi()
-                        .Build())
+                        .AddMvcCore()
+                        .AddNewtonsoftJson())
                     .Configure(app => app
                         .UseRouting()
-                        .UseEndpoints(r => r.MapControllers())
-                        .UseDispatcherEndpoints(endpoints => endpoints
-                            .Get("", ctx => ctx.Response.WriteAsync("Orders Service"))
-                            .Get<GetOrder, OrderDto>("orders/{orderId}")
-                            .Post<CreateOrder>("orders",
-                                afterDispatch: (cmd, ctx) => ctx.Response.Created($"orders/{cmd.OrderId}")))
-                        .UseConsul()
-                        .UseJaeger()
-                        .UseInitializers()
-                        .UseMetrics()
-                        .UseErrorHandler()
-                        .UseRabbitMq()
-                        .SubscribeEvent<DeliveryStarted>())
+                        .UseEndpoints(r => r.MapControllers()))
                     .UseLogging();
             });
     }
